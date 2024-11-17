8f14fe0959df9888429440081194cec00d6889b7
rakesh <rakesh_anvekar@gmail.com>
2024-11-17 18:20:33 +0530

    build issue fix

--- taskmanagementapp.client/src/Helpers/Ioc/IocHelper.ts
+++ taskmanagementapp.client/src/Helpers/Ioc/IocHelper.ts
@@ -1,18 +1,9 @@
 import { ITaskItemRepository } from "../../Repositories/TaskItem/ITaskItemRepository";
 import TaskItemRepository from "../../Repositories/TaskItem/TaskItemRepository";
-import RepositoryHelper from "../Repository/RepositoryHelper";
 
 export default class IocHelper{
     private static taskItemRepository:ITaskItemRepository | null = null;
-    private static repositoryHelper:IRepositoryHelper|null=null;
 ;
-    public readonly getRepositoryHelper=():IRepositoryHelper=>{
-        if(IocHelper.repositoryHelper!==null) return IocHelper.repositoryHelper;
-
-        IocHelper.repositoryHelper= new RepositoryHelper();
-        return IocHelper.repositoryHelper;
-    }
-
     public readonly getTaskItemRepository=():ITaskItemRepository=>{
         if(IocHelper.taskItemRepository!==null)
             return IocHelper.taskItemRepository;
